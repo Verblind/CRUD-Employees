eeaae5c86548aab8635f4c4c2b34fc6ffd7da76c
Jeremy De Lange <jeremy.delange@liebengroup.co.za>
2024-06-06 11:28:15 +0200

    test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,90 +1,61 @@
 ﻿using System;
-using System.ComponentModel.Design;
 
-namespace HelloWorld
-{/*Multi Line Comments
-  * Used to put everything inbetween as a comment
-  */
+namespace CRUD
+{
     class Program
     {
+        static string[] employees = new string[] { "Jeremy de Lange", "Alexander Lee" };
+
         static void Main(string[] args)
         {
-            //Declarations
-            string[] options = {"Create", "Read", "Update", "Delete","Exit"};
+
+            string[] options = { "Create", "Read", "Update", "Delete", "Exit" };
             string input;
-            String[] employees = new string[100];
+
             bool Continue = true;
 
 
-            //Input
             while (Continue)
             {
                 Console.WriteLine("");
                 Console.WriteLine("What would you like to do");
-                    for (int identifier = 0; identifier < options.Length;identifier++)
-                      {
-                            Console.WriteLine(options[identifier]);
-                      }
+                for (int identifier = 0; identifier < options.Length; identifier++)
+                {
+                    Console.WriteLine(options[identifier]);
+                }
                 Console.Write("Selection: ");
-                     input = Console.ReadLine();
+                input = Console.ReadLine().ToUpper();
 
                 switch (input)
                 {
-                    case "Create":
-                        Console.Write("Enter employee name: ");
-                        String newemployee = Console.ReadLine();
+                    case "CREATE":
 
-                        for (int i = 0; i < employees.Length; i++)
-                        {
-                            if (employees[i] == null)
-                            {
-                                employees[i] = newemployee;
-                                Console.WriteLine("Employee added as :Index["+Convert.ToString(i) + "] "+newemployee);
-                                return;
-                            }
-                       
-                        }
-                       
+                        Create();
+
                         break;
-                    case "Read":
-                        foreach (string employee in employees)
-                        {
-                            Console.WriteLine("Employee Name: " + employee);
-                        }
+
+                    case "READ":
+
+                        MRead();
+
                         break;
-                    case "Update":
-                        Console.WriteLine("Which Employee name would you like to update?( Select the index between 0 and "+ Convert.ToString(employees.Length-1));
-                        int Uindex = 0;
-                        foreach (string employee in employees)
-                        {
-                            Console.WriteLine("Employee Name: Index["+ Convert.ToString(Uindex) + "] employee");
-                            Uindex++;
-                        }
-                        Console.Write("Index: ");
-                        Uindex = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("You have selected " + employees[Uindex] + ", Please type the name you would like to change it to.");
-                            string oldname = employees[Uindex];
-                        Console.Write("New Name: ");
-                            employees[Uindex] = Console.ReadLine();
-                        Console.WriteLine("You have changed " + oldname + " to " + employees[Uindex]);
+
+                    case "UPDATE":
+
+                        MUpdate();
+
                         break;
-                    case "Delete":
-                        Console.WriteLine("Which Employee would you like to remove?( Select the index between 0 and " + Convert.ToString(employees.Length - 1));
-                        Uindex = 0;
-                        foreach (string employee in employees)
-                        {
-                            Console.WriteLine("Employee Name: " + Convert.ToString(Uindex) + " " + employee);
-                            Uindex++;
-                        }
-                        Console.Write("Index: ");
-                        Uindex = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("You have selected " + employees[Uindex] + ", Please confirm if you would like to delete this Employee from the records");
-                        oldname = employees[Uindex];
-                        employees[Uindex] = null;
-                        Console.WriteLine("You have Deleted " + oldname + " from the System " );
+
+                    case "DELETE":
+
+                        Delete();
+
                         break;
-                    case "Exit":
-                        Continue= false;
+
+                    case "EXIT":
+
+                        Continue = false;
+
                         break;
                     default:
                         Console.WriteLine($"{input} is an invalid selection");
@@ -95,7 +66,64 @@ namespace HelloWorld
             }
 
         }
+
+        static void Create()
+        {
+            int newIndex = employees.Length + 1;
+            Console.Write("Enter employee name: ");
+            String newemployee = Console.ReadLine();
+
+
+            employees = employees.Append(newemployee).ToArray();
+
+            Console.WriteLine(newemployee + " has been added");
+
+        }
+
+        static void MRead()
+        {
+            foreach (string employee in employees)
+            {
+                Console.WriteLine("Employee Name: " + employee);
+            }
+        }
+        static void MUpdate()
+        {
+            Console.WriteLine("Which Employee name would you like to update?( Select the index between 0 and " + Convert.ToString(employees.Length));
+            int Uindex = 0;
+            foreach (string employee in employees)
+            {
+                Console.WriteLine("Employee Name: Index[" + Convert.ToString(Uindex) + "] " + employee);
+                Uindex++;
+            }
+            Console.Write("Index: ");
+            Uindex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have selected " + employees[Uindex] + ", Please type the name you would like to change it to.");
+            string oldname = employees[Uindex];
+            Console.Write("New Name: ");
+            employees[Uindex] = Console.ReadLine();
+            Console.WriteLine("You have changed " + oldname + " to " + employees[Uindex]);
+            return;
+        }
+
+        static void Delete()
+        {
+            Console.WriteLine("which employee would you like to remove?( select the index between 0 and " + Convert.ToString(employees.Length - 1));
+            int uindex = 0;
+            foreach (string employee in employees)
+            {
+                Console.WriteLine("employee name: " + Convert.ToString(uindex) + " " +employee);
+                uindex++;
+            }
+            Console.Write("index: ");
+            uindex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Employee [" + Convert.ToString(uindex) + "] " + employees[uindex] + " Has been Deleted");
+            employees = employees.Where((x, i) => uindex != i).ToArray();
+
+            //test
+
+        }
+
     }
 }
 
-           
